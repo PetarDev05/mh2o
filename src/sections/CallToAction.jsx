1a50b60433f599195b1f9c9906d24627d4b336ca
PetarDev05 <pejovicpetar02@gmail.com>
2026-06-09 13:53:06 +0200

    Add hover animation to the Call section.

--- src/sections/CallToAction.jsx
+++ src/sections/CallToAction.jsx
@@ -22,13 +22,13 @@ const CallToAction = () => {
         <div className="flex flex-col min-[450px]:flex-row items-center min-[450px]:items-center gap-5">
           <Link
             to="/saradnja#kontakt"
-            className="flex items-center gap-2 px-7 py-2 rounded-full bg-linear-to-r from-(--primary) to-(--secondary) text-(--button-text) font-semibold text-[13px] cursor-pointer"
+            className="flex items-center gap-2 px-7 py-2 rounded-full bg-linear-to-r from-(--primary) to-(--secondary) text-(--button-text) font-semibold text-[13px] cursor-pointer hover:scale-105 duration-150"
           >
             Počni odmah <FaArrowRightLong className="text-(--button-text)" />
           </Link>
           <Link
             to="/saradnja#planovi"
-            className="flex items-center gap-2 px-7 py-2 rounded-full border-2 border-(--primary) text-(--button-text) font-semibold text-[13px] cursor-pointer"
+            className="flex items-center gap-2 px-7 py-2 rounded-full border-2 border-(--primary) text-(--button-text) font-semibold text-[13px] cursor-pointer hover:scale-105 duration-150"
           >
             Pogledaj planove{" "}
             <FaArrowRightLong className="text-(--button-text)" />
